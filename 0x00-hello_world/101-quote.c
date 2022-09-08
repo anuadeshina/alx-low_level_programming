1d50c73f42a80021d80e7eacdbfe82a6b96f3619
Anu Ajayi <anuadeshina93@yahoo.com>
2022-09-08 15:30:04 -0700

    Task 8 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Prints phrase and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * Return: Always 1
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",
-		59);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }

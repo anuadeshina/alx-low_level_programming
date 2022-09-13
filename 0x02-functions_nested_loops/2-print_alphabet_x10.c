e7c97c5d45c47579b1dbb3e5571f0dd03d0735bc
Anu Ajayi <anuadeshina93@yahoo.com>
2022-09-13 13:05:34 -0700

    More functions tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,3 +2,24 @@
 
 /**
  * main - Prints 10 times the alphabet, followed by a new line
+ * Return: Always 0 on success
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabet_count = 0;
+
+	while (alphabet_count < 10)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		alphabet_count++;
+		_putchar('\n');
+	}
+
+}
